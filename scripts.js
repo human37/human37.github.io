068dea888ead70d91e223030a58237645098d539
Ammon Taylor <ammonx9@gmail.com>
2020-07-02 09:43:31 -0600

    added UDOT Traffic App Downloads

--- scripts.js
+++ scripts.js
@@ -6,6 +6,7 @@ google.charts.load('current', {
 google.charts.setOnLoadCallback(getData1);
 google.charts.setOnLoadCallback(getData2);
 google.charts.setOnLoadCallback(getData3);
+google.charts.setOnLoadCallback(getData4);
 
 function getData1() {
     var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1V8PO4BdZbR3tNM04JJWf_afLrX8Vm2MbU-Sa6eKcIrw/edit#gid=496119454');
@@ -107,5 +108,36 @@ function handleDataResponse3(response) {
             });
     MaintenanceTrafficSignalsChart.setView({'columns':[0,1,2]})
     dashboard.bind(regionFilter, MaintenanceTrafficSignalsChart);
+    dashboard.draw(data);
+}
+function getData4() {
+    var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1V8PO4BdZbR3tNM04JJWf_afLrX8Vm2MbU-Sa6eKcIrw/gviz/tq?sheet=udot_traffic_downloads');
+    query.setQuery('SELECT A, B, C')
+    query.send(handleDataResponse4);
+}
+function handleDataResponse4(response) {
+    if (response.isError()) {
+        alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+        return;
+    }
+    var data = response.getDataTable();
+    var dashboard = new google.visualization.Dashboard(document.getElementById('programmatic_dashboard_div_4'));
+    var dateFilter = new google.visualization.ControlWrapper({
+        'controlType': 'DateRangeFilter',
+        'containerId': 'control_filter_4',
+        'options': {
+            'filterColumnLabel': 'Date'
+            }
+        });
+    var MaintenanceTrafficSignalsChart  = new google.visualization.ChartWrapper({
+        'chartType': 'ColumnChart',
+        'containerId': 'dynamic_chart_4',
+        'options': {
+            title: 'UDOT Traffic App: Cummulative Downloads By Type	',
+            width: 1200,
+            height: 500,
+            isStacked: true}
+            });
+    dashboard.bind(dateFilter, MaintenanceTrafficSignalsChart);
     dashboard.draw(data);
 }
