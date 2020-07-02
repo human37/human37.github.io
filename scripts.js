943d8b7c2640f503333727a0c2d0f3824cbc651b
Ammon Taylor <ammonx9@gmail.com>
2020-07-02 11:35:11 -0600

    added both category filter charts

--- scripts.js
+++ scripts.js
@@ -7,6 +7,8 @@ google.charts.setOnLoadCallback(getData1);
 google.charts.setOnLoadCallback(getData2);
 google.charts.setOnLoadCallback(getData3);
 google.charts.setOnLoadCallback(getData4);
+google.charts.setOnLoadCallback(getData5);
+google.charts.setOnLoadCallback(getData6);
 
 function getData1() {
     var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1V8PO4BdZbR3tNM04JJWf_afLrX8Vm2MbU-Sa6eKcIrw/edit#gid=496119454');
@@ -140,4 +142,130 @@ function handleDataResponse4(response) {
             });
     dashboard.bind(dateFilter, MaintenanceTrafficSignalsChart);
     dashboard.draw(data);
+}
+function getData5() {
+    var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1V8PO4BdZbR3tNM04JJWf_afLrX8Vm2MbU-Sa6eKcIrw/gviz/tq?sheet=customer_service_maintenance');
+    query.setQuery('SELECT A, B, C, D, E, F, G, H, I, J, K, L, M')
+    query.send(handleDataResponse5);
+}
+function handleDataResponse5(response) {
+    if (response.isError()) {
+        alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+        return;
+    }
+    var data = response.getDataTable();
+    var columnsTable = new google.visualization.DataTable();
+    columnsTable.addColumn('number', 'colIndex');
+    columnsTable.addColumn('string', 'colLabel');
+    var initState= {selectedValues: []};
+    // put the columns into this data table (skip column 0)
+    for (var i = 1; i < data.getNumberOfColumns(); i++) {
+        columnsTable.addRow([i, data.getColumnLabel(i)]);
+    }
+    initState.selectedValues.push(data.getColumnLabel(7));
+    var NewWorkOrdersChart  = new google.visualization.ChartWrapper({
+        'chartType': 'ColumnChart',
+        'containerId': 'dynamic_chart_5',
+        'dataTable' : data,
+        'options': {
+            title: 'Maintenance: New Work Orders By Work Group',
+            width: 1200,
+            height: 500,
+            isStacked: true}
+            });
+    var columnFilter = new google.visualization.ControlWrapper({
+        controlType: 'CategoryFilter',
+        containerId: 'control_filter_5',
+        dataTable: columnsTable,
+        options: {
+            filterColumnLabel: 'colLabel',
+            ui: {
+                label: 'Work Group',
+                allowMultiple: true,
+                allowTyping: false
+            }
+        },
+        state: initState
+    });
+    function setChartView() {
+        var state = columnFilter.getState();
+        var row;
+        var columnIndices = [0];
+        for (var i = 0; i < state.selectedValues.length; i++) {
+            row = columnsTable.getFilteredRows([{column: 1, value: state.selectedValues[i]}])[0];
+            columnIndices.push(columnsTable.getValue(row, 0));
+        }
+        // sort the indices into their original order
+        columnIndices.sort(function (a, b) {
+            return (a - b);
+        });
+        NewWorkOrdersChart.setView({columns: columnIndices});
+        NewWorkOrdersChart.draw();
+    };
+    google.visualization.events.addListener(columnFilter, 'statechange', setChartView);
+    setChartView();
+    columnFilter.draw();
+}
+function getData6() {
+    var query = new google.visualization.Query('https://docs.google.com/spreadsheets/d/1V8PO4BdZbR3tNM04JJWf_afLrX8Vm2MbU-Sa6eKcIrw/gviz/tq?sheet=work_order_average_time');
+    query.setQuery('SELECT A, B, C, D, E, F')
+    query.send(handleDataResponse6);
+}
+function handleDataResponse6(response) {
+    if (response.isError()) {
+        alert('Error in query: ' + response.getMessage() + ' ' + response.getDetailedMessage());
+        return;
+    }
+    var data = response.getDataTable();
+    var columnsTable = new google.visualization.DataTable();
+    columnsTable.addColumn('number', 'colIndex');
+    columnsTable.addColumn('string', 'colLabel');
+    var initState= {selectedValues: []};
+    // put the columns into this data table (skip column 0)
+    for (var i = 1; i < data.getNumberOfColumns(); i++) {
+        columnsTable.addRow([i, data.getColumnLabel(i)]);
+        initState.selectedValues.push(data.getColumnLabel(i));
+    }
+    var NewWorkOrdersChart  = new google.visualization.ChartWrapper({
+        'chartType': 'ColumnChart',
+        'containerId': 'dynamic_chart_6',
+        'dataTable' : data,
+        'options': {
+            title: 'Maintenance: Average Days To Close Work Orders By Work Group',
+            width: 1200,
+            height: 500,
+            isStacked: true}
+            });
+    var columnFilter = new google.visualization.ControlWrapper({
+        controlType: 'CategoryFilter',
+        containerId: 'control_filter_6',
+        dataTable: columnsTable,
+        options: {
+            filterColumnLabel: 'colLabel',
+            ui: {
+                label: 'Work Group',
+                allowMultiple: true,
+                allowTyping: false
+            }
+        },
+        state: initState
+    });
+    function setChartView() {
+        var state = columnFilter.getState();
+        var row;
+        var columnIndices = [0];
+        for (var i = 0; i < state.selectedValues.length; i++) {
+            row = columnsTable.getFilteredRows([{column: 1, value: state.selectedValues[i]}])[0];
+            columnIndices.push(columnsTable.getValue(row, 0));
+        }
+        // sort the indices into their original order
+        columnIndices.sort(function (a, b) {
+            return (a - b);
+        });
+        NewWorkOrdersChart.setView({columns: columnIndices});
+        NewWorkOrdersChart.draw();
+    };
+    google.visualization.events.addListener(columnFilter, 'statechange', setChartView);
+    setChartView();
+    columnFilter.draw();
 }
